a766cec31fac2ecbcaf2fe30fbf1d29350ee5047
liweiye <1101931365@qq.com>
2020-07-28 11:07:53 +0800

    a

---
+++ te.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lwy
+ * Date: 2020/7/26
+ * Time: 16:51
+ */
+
+for($i=0;$i<50;$i++){
+    include "include.php";
+    echo $a.PHP_EOL;
+    sleep(3);
+}
